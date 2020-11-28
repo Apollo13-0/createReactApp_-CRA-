77120c61a4b3091a4c6c2071543a92f65e33821a
Apollo13 <calderon.ig08@estudiantec.cr>
2020-11-27 19:12:08 -0600

    add first version

--- star-wars-app/src/App.js
+++ star-wars-app/src/App.js
@@ -8,6 +8,7 @@ export default class App extends React.Component {
         super(props);
         this.state = {
             counter: 0
+            characters: []
         }
     }
     componentDidMount() {
@@ -42,6 +43,44 @@ export default class App extends React.Component {
                     </a>
                     <div>{this.state.counter}</div>
                     <Alert count={this.state.counter}></Alert>
+                    <table border = "1">
+                        <tr>
+                            <td>Name</td>
+                            <td>height</td>
+                            <td>mass</td>
+                            <td>hair_color</td>
+                            <td>skin_color</td>
+                            <td>Neye_colorame</td>
+                            <td>birth_year</td>
+                            <td>gender</td>
+                            <td>homeworld</td>
+                            <td>films</td>
+                            <td>species</td>
+                            <td>vehicles</td>
+                            <td>starships</td>
+                            <td>created</td>
+                            <td>edited</td>
+                            <td>url</td>
+                        </tr>
+                        <tr>
+                            <td>{this.characters[0].name}</td>
+                            <td>{this.characters[0].height}</td>
+                            <td>{this.characters[0].mass}</td>
+                            <td>{this.characters[0].hair_color}</td>
+                            <td>{this.characters[0].skin_color}</td>
+                            <td>{this.characters[0].Neye_colorame}</td>
+                            <td>{this.characters[0].birth_year}</td>
+                            <td>{this.characters[0].gender}</td>
+                            <td>{this.characters[0].homeworld}</td>
+                            <td>{this.characters[0].films}</td>
+                            <td>{this.characters[0].species}</td>
+                            <td>{this.characters[0].vehicles}</td>
+                            <td>{this.characters[0].starships}</td>
+                            <td>{this.characters[0].created}</td>
+                            <td>{this.characters[0].edited}</td>
+                            <td>{this.characters[0].url}</td>
+                        </tr>
+                    </table>
                 </header>
             </div>
         );
